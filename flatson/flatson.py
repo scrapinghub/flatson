e1e3e3929751ef76c3ed87080e580ed7f67b2676
Elias Dorneles <eliasdorneles@gmail.com>
2015-07-10 20:57:26 -0300

    added support for nested objects

--- flatson/flatson.py
+++ flatson/flatson.py
@@ -1,14 +1,53 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+from collections import namedtuple
+
 import json
 
 
-class Flatson(object):
-    def __init__(self, schema):
-        self.schema = schema
-        self.fieldnames = self._build_fieldnames()
+Field = namedtuple('Field', 'name getter')
 
-    def _build_fieldnames(self):
-        return [k for k in self.schema.get('properties', {})]
+
+def create_getter(path, field_sep='.'):
+    if field_sep in path:
+        first_key, rest = path.split(field_sep, 1)
+        return lambda x: create_getter(rest)(x.get(first_key, {}))
+    else:
+        return lambda x: x.get(path, None)
+
+
+def infer_flattened_field_names(schema, field_sep='.'):
+    fields = []
+
+    for key, value in schema.get('properties', {}).items():
+        # TODO: add support for configuration through schema
+        val_type = value.get('type')
+        if val_type == 'object':
+            for subfield, _ in infer_flattened_field_names(value):
+                full_name = '{prefix}{fsep}{extension}'.format(
+                    prefix=key, fsep=field_sep, extension=subfield)
+                fields.append(Field(full_name, create_getter(full_name)))
+        else:
+            fields.append(Field(key, create_getter(key)))
+
+    return sorted(fields)
+
+
+class Flatson(object):
+    def __init__(self, schema, field_sep='.'):
+        self.schema = schema
+        self.field_sep = field_sep
+        self.fields = self._build_fields()
+
+    @property
+    def fieldnames(self):
+        return [f.name for f in self.fields]
+
+    def _build_fields(self):
+        if self.schema.get('type') != 'object':
+            raise ValueError("Schema should be of type object")
+        return infer_flattened_field_names(self.schema,
+                                           field_sep=self.field_sep)
 
     @classmethod
     def from_schemafile(cls, schemafile):
@@ -16,4 +55,4 @@ class Flatson(object):
             return cls(json.load(f))
 
     def flatten(self, obj):
-        return [obj.get(p) for p in self.fieldnames]
+        return [f.getter(obj) for f in self.fields]
--- tests/test_flatson.py
+++ tests/test_flatson.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
 
 """
 test_flatson
@@ -10,19 +11,18 @@ Tests for `flatson` module.
 
 import json
 import os
+import skinfer
 import unittest
 
 from flatson import Flatson
 import tempfile
 
 
-SIMPLE_SCHEMA = {
-    '$schema': u'http://json-schema.org/draft-04/schema',
-    'type': 'object',
-    'properties': {
-        'a_prop': {'type': 'string'},
-    },
-}
+EMPTY_SCHEMA = skinfer.generate_schema({})
+
+SIMPLE_SCHEMA = skinfer.generate_schema({'a_prop': ''})
+
+LIST_SCHEMA = skinfer.generate_schema([])
 
 
 class TestFlatson(unittest.TestCase):
@@ -41,17 +41,52 @@ class TestFlatson(unittest.TestCase):
         finally:
             os.remove(fname)
 
+    def test_no_support_for_list_objects(self):
+        with self.assertRaises(ValueError):
+            Flatson(schema=LIST_SCHEMA)
+
     def test_when_no_declared_properties_flatten_empty_list(self):
-        schema = SIMPLE_SCHEMA.copy()
-        del schema['properties']
-        f = Flatson(schema=schema)
+        f = Flatson(schema=EMPTY_SCHEMA)
         result = f.flatten({'a_prop': 'a_value'})
         self.assertEquals([], result)
 
-    def test_convert_simple_object(self):
+    def test_convert_simple_objects(self):
         f = Flatson(schema=SIMPLE_SCHEMA)
-        result = f.flatten({'a_prop': 'a_value'})
-        self.assertEquals(['a_value'], result)
+        self.assertEquals(['a_prop'], f.fieldnames)
+        self.assertEquals(['a_value'], f.flatten({'a_prop': 'a_value'}))
+        self.assertEquals([None], f.flatten({}))
+
+    def test_convert_nested_objects(self):
+        contain_nested_object = {
+            'first': 'hello',
+            'second': {
+                'one': 1,
+                'two': 2,
+            }
+        }
+        schema = skinfer.generate_schema(contain_nested_object)
+        f = Flatson(schema=schema)
+        self.assertEquals(['first', 'second.one', 'second.two'], f.fieldnames)
+        self.assertEquals(['hello', 1, 2], f.flatten(contain_nested_object))
+
+    def test_convert_deep_nested_objects(self):
+        contain_nested_object = {
+            'first': 'hello',
+            'second': {
+                'one': {
+                    'a': 1,
+                    'b': 2,
+                },
+                'two': {
+                    'a': 3,
+                    'b': 4,
+                },
+            }
+        }
+        schema = skinfer.generate_schema(contain_nested_object)
+        f = Flatson(schema=schema)
+        self.assertEquals(['first', 'second.one.a', 'second.one.b', 'second.two.a', 'second.two.b'], f.fieldnames)
+        self.assertEquals(['hello', 1, 2, 3, 4], f.flatten(contain_nested_object))
 
 if __name__ == '__main__':
     unittest.main()
